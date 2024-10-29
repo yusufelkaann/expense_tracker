a8365344d6ca5e0b9123463f9b518e8fb849095e
Yusuf Elkaan <elkaanyusuff@gmail.com>
2024-10-29 18:04:53 +0300

    architecture and bottom navbar

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:expense_tracker/app.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(const MyApp());
+}
---
+++ lib/pages/home/views/home_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 8,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.stacked_bar_chart),
+              label: 'Stats',
+            ),
+          ]
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+
+        shape: const CircleBorder(),
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.tertiary,
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.primary,
+              ],
+              transform: const GradientRotation(pi / 4),
+            )
+          ),
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
+      ),
+    );
+  }
+}
